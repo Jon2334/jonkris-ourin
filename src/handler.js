2f3554f9102cd71cd1519abd56ec7c58d5bf750a
jon kritiansah silaban <silabancris949@gmail.com>
2026-02-16 14:11:56 +0700

    Update codingan terbaru

--- src/handler.js
+++ src/handler.js
@@ -1,21 +1,4 @@
-/**
- * Credits & Thanks to
- * Developer = Lucky Archz ( Zann )
- * Lead owner = HyuuSATAN
- * Owner = Keisya
- * Owner = Syura Salsabila
- * Designer = Danzzz
- * Wileys = Penyedia baileys
- * Penyedia API
- * Penyedia Scraper
- *
- * JANGAN HAPUS/GANTI CREDITS & THANKS TO
- * JANGAN DIJUAL YA MEK
- *
- * Saluran Resmi Ourin:
- * https://whatsapp.com/channel/0029VbB37bgBfxoAmAlsgE0t
- *
- */
+
 
 const config = require("../config");
 const { isSelf } = require("../config");
